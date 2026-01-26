fd4c4447e34df9a0607610e3c37c9fa42557c10a
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-01-27 01:02:15 +0530

    Time: 0 ms (100%), Space: 21.9 MB (37.28%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,11 +2,28 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        int sum=(n*(n+1))/2;
-        int arr=0;
-        for(int i=0;i<nums.size();i++){
-            arr+=nums[i];
+        //method 1 O(n) me 
+        // int sum=(n*(n+1))/2;
+        // int arr=0;
+        // for(int i=0;i<nums.size();i++){
+        //     arr+=nums[i];
+        // }
+        // return sum-arr;
+        //method 2 O(log n).....
+        sort(nums.begin(),nums.end());
+    int left=0,right=n-1;
+    int missing =n;
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==mid){
+            left=mid+1;
         }
-        return sum-arr;
+        else{
+            missing=mid;
+            right=mid-1;
+        }
     }
+    return missing;
+    }
+    
 };
