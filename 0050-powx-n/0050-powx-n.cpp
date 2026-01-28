09196243c586c2532890125e3b7b76c30c6550fe
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-01-28 14:27:43 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.17%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        long bin=n;
-        double ans=1;
-        if(n<0){
-            x=1/x;
-            bin=-bin;
+    double myPow(double a, int b) {
+        long long n = b;
+        if (n < 0) {
+            a = 1 / a;
+            n = -n;
         }
-        while(bin>0){
-            if(bin%2==1){
-                ans*=x;
-            }
-            x*=x;
-            bin/=2;
-        }
-        return ans;
+        return power(a, n);
+    }
+
+    double power(double a, long long n) {
+        if (n == 0) return 1;
+
+        double half = power(a, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return a * half * half;
     }
 };
