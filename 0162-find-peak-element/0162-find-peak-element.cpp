1f1ca30c0d97bf27a9be0254a99210290d6c05bf
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-01-31 11:02:02 +0530

    Time: 0 ms (100%), Space: 12.5 MB (86.26%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+//   Binary search approach will work here  divide and thechoose the 
+    int findPeakElement(vector<int>& arr) {
+        int n=arr.size();
+        if(n==1)return 0;
+        if(arr[0]>=arr[1])return 0;
+        if(arr[n-1]>=arr[n-2])return n-1;
+        for(int i=1;i<arr.size()-1;i++){
+            if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1])return i;
+        }
+        return -1;
+    }
+};
