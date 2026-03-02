59187f4431f01c45a47f4d66d342b731d5d5f7aa
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-03-02 09:20:04 +0530

    Time: 0 ms (100%), Space: 30.1 MB (41.46%) - LeetHub

---
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minSwaps(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> zeros(n);
+        
+        // Count trailing zeros for each row
+        for(int i = 0; i < n; i++) {
+            int count = 0;
+            for(int j = n - 1; j >= 0; j--) {
+                if(grid[i][j] == 0)
+                    count++;
+                else
+                    break;
+            }
+            zeros[i] = count;
+        }
+        
+        int swaps = 0;
+        
+        for(int i = 0; i < n; i++) {
+            int required = n - i - 1;
+            int j = i;
+            
+            // Find suitable row
+            while(j < n && zeros[j] < required)
+                j++;
+            
+            if(j == n)
+                return -1;
+            
+            // Bring row j to position i
+            while(j > i) {
+                swap(zeros[j], zeros[j - 1]);
+                swaps++;
+                j--;
+            }
+        }
+        
+        return swaps;
+    }
+};
