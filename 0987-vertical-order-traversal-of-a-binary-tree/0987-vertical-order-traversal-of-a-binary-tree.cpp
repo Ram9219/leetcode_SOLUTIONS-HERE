e02cbd7abac63acb2dbaf51cc7b3aa5af2747e99
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-03-18 14:35:32 +0530

    Time: 1 ms (55.83%), Space: 15.6 MB (77.52%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,34 +12,37 @@
  */
 class Solution {
 public:
+    // use the queuee and the map to store the order of the vertical nodes
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>> ans;
-        map<int, map<int, multiset<int>>> mp;
-        //{root},{col,row}.......
+        if (!root)
+            return ans;
+        // make a queue {root},{col,row}
         queue<pair<TreeNode*, pair<int, int>>> q;
-        q.push({root, {0, 0}});
+        // now mak ea map to store and hanle the one by one vertical nodes
+        map<int, map<int, multiset<int>>> mp;
+        q.push({root,{0,0}}); // intially passing the 0 row and 0 col and root as null
         while (!q.empty()) {
-            auto front = q.front();
+            auto front=q.front();
             q.pop();
-            TreeNode* node = front.first;
-            int col = front.second.first;
-            int row = front.second.second;
+            TreeNode*node=front.first;
+            int col=front.second.first;
+            int row=front.second.second;
             mp[col][row].insert(node->val);
-            if (node->left)
-                q.push({node->left, {col - 1, row + 1}});
-            if (node->right)
-                q.push({{node->right}, {col + 1, row + 1}});
+            //aaab left  me jaake dekh 
+            if(node->left)q.push({{node->left},{col-1,row+1}});
+            if(node->right)q.push({{node->right},{col+1,row+1}});
         }
-        // now ham ab ans ko build karenge
-        for (auto& colPair : mp) {
-            vector<int> temp;
-
-            for (auto& rowPair : colPair.second) {
-                temp.insert(temp.end(), rowPair.second.begin(),
-                            rowPair.second.end());
+        //now just we have to build the ans 
+        for(auto&colpair:mp){
+            vector<int>col;
+            for(auto& rowpair:colpair.second){
+                col.insert(col.end(),
+                rowpair.second.begin(),
+                rowpair.second.end()
+                );
             }
-
-            ans.push_back(temp);
+            ans.push_back(col);
         }
         return ans;
     }
