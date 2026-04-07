b418efe425b4b46d446b1f363d21a994ceba8ee6
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-04-07 22:01:02 +0530

    Time: 0 ms (100%), Space: 19.4 MB (83.44%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    // for(auto&x:adj[node]){
+    //     if(!vis[x]){
+    //         dfs(x,adj,vis);
+    //     }
+    // }//maine directly adjacency list use kar liya is wjah se ye nahi chalega
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
+        vis[node] = 1;
+        for (int i = 0; i < adj.size(); i++) {
+            if (adj[node][i] == 1 && !vis[i]) {
+                dfs(i, adj, vis);
+            }
+        }
+    }
+
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> vis(n, 0);
+        // vis[0]=1;
+        int count = 0;
+        for (int i = 0; i < isConnected.size(); i++) {
+            if (!vis[i]) {
+                count++;
+                dfs(i, isConnected, vis);
+            }
+        }
+        return count;
+    }
+};
