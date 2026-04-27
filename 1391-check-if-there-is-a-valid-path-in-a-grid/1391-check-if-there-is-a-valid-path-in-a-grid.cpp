43a2bcabe4524a1cdfae82c6dcc02c94dee16b41
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-04-28 02:30:54 +0530

    Time: 275 ms (6.88%), Space: 121.2 MB (5.26%) - LeetHub

---
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool hasValidPath(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int>>> dirs = {{},
+                                            {{0, -1}, {0, 1}},
+                                            {{-1, 0}, {1, 0}},
+                                            {{0, -1}, {1, 0}},
+                                            {{0, 1}, {1, 0}},
+                                            {{0, -1}, {-1, 0}},
+                                            {{0, 1}, {-1, 0}}};
+
+        queue<pair<int, int>> q;
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+        q.push({0, 0});
+        vis[0][0] = true;
+
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            if (r == n - 1 && c == m - 1)
+                return true;
+
+            for (auto d : dirs[grid[r][c]]) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr < 0 || nc < 0 || nr >= n || nc >= m || vis[nr][nc])
+                    continue;
+                for (auto back : dirs[grid[nr][nc]]) {
+                    if (nr + back[0] == r && nc + back[1] == c) {
+                        vis[nr][nc] = true;
+                        q.push({nr, nc});
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+};
