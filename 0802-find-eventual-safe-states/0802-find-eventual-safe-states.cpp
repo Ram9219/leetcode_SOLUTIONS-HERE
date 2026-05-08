b194ee9fb80af324d01a09733afbb544402adbbf
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-05-08 20:05:33 +0530

    Time: 51 ms (40.51%), Space: 66.3 MB (35.9%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<vector<int>>adj(n);
-        vector<int>indeg(n,0);
-        //reverse karna  hai 
-        for(int i=0;i<n;i++){
-            //i->it
-            //it->i karna hai 
-            for(auto&it:graph[i]){
+        int n = graph.size();
+        vector<vector<int>> adj(n);
+        vector<int> indeg(n, 0);
+        // reverse karna  hai
+        for (int i = 0; i < n; i++) {
+            // i->it
+            // it->i karna hai
+            for (auto& it : graph[i]) {
                 adj[it].push_back(i);
                 indeg[i]++;
             }
         }
-        queue<int>q;
-        for(int i=0;i<n;i++){
-            if(indeg[i]==0){
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indeg[i] == 0) {
                 q.push(i);
             }
         }
-        vector<int>ans;
-        while(!q.empty()){
-            int node=q.front();
+        vector<int> ans;
+        while (!q.empty()) {
+            int node = q.front();
             q.pop();
             ans.push_back(node);
-            for(auto&it:adj[node]){
+            for (auto& it : adj[node]) {
                 indeg[it]--;
-                if(indeg[it]==0){
+                if (indeg[it] == 0) {
                     q.push(it);
                 }
             }
         }
-        sort(begin(ans),end(ans));
+        sort(begin(ans), end(ans));
         return ans;
     }
 };
