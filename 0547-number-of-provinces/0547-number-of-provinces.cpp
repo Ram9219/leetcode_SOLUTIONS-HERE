964138c69a47d787b83130de55878ec510cc6711
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-05-26 16:57:12 +0530

    Time: 2 ms (42.14%), Space: 19.3 MB (92.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,43 +1,47 @@
+class DisjointSet{
+public:
+    vector<int>rank,parent;
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    int ult_parent(int node){
+        if(node==parent[node])return node;
+        return parent[node]=ult_parent(parent[node]);
+    }
+    void find_Union(int u,int v){
+        int pu=ult_parent(u);
+        int pv=ult_parent(v);
+        if(rank[pu]<rank[pv]){
+            parent[pu]=pv;
+        }
+        else if(rank[pv]<rank[pu]){
+            parent[pv]=pu;
+        }else{
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+    }
+};
 class Solution {
-private:
-    // for(auto&x:adj[node]){
-    //     if(!vis[x]){
-    //         dfs(x,adj,vis);
-    //     }
-    // }//maine directly adjacency list use kar liya is wjah se ye nahi chalega
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
-        vis[node] = 1;
-        for (int i = 0; i < adj.size(); i++) {
-            if (adj[node][i] == 1 && !vis[i]) {
-                dfs(i, adj, vis);
-            }
-        }
-    }
-void bfs(int node, vector<vector<int>>& adj, vector<int>& vis){
-    vis[node]=1;
-    queue<int>q;
-    q.push(node);
-    while(!q.empty()){
-        int cur=q.front();
-        q.pop();
-        for(int i=0;i<adj.size();i++){
-            if(!vis[i]&& adj[cur][i]==1){
-              vis[i]=1;
-              q.push(i);
-            }
-        }
-    }
-}
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> vis(n, 0);
-        // vis[0]=1;
-        int count = 0;
-        for (int i = 0; i < isConnected.size(); i++) {
-            if (!vis[i]) {
+        int n=isConnected.size();
+        DisjointSet d(n);
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    d.find_Union(i,j);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(d.ult_parent(i)==i){
                 count++;
-                bfs(i, isConnected, vis);
             }
         }
         return count;
