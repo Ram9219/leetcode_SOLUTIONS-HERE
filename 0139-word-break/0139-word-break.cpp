13e0d5042e4f67d0276659c43e3b3006d2ddbc5c
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-06-19 15:32:30 +0530

    Time: 3 ms (73.25%), Space: 13 MB (63.42%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,56 @@
+class Node {
+public:
+    Node* child[26];
+    bool isend;
+    Node() {
+        isend = false;
+        for (int i = 0; i < 26; i++) {
+            child[i] = NULL;
+        }
+    }
+};
+
+class Trie {
+public:
+    Node* root;
+    Trie() { root = new Node(); }
+    void insert(string word) {
+        Node* node = root;
+        for (char ch : word) {
+            int i = ch - 'a';
+            if (node->child[i] == NULL) {
+                node->child[i] = new Node();
+            }
+            node = node->child[i];
+        }
+        node->isend = true;
+    }
+};
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        Trie t;
+        int n = s.size();
+        for (auto& ch : wordDict) {
+            t.insert(ch);
+        }
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            Node* node = t.root;
+            if (!dp[i])
+                continue;
+            for (int j = i; j < n; j++) {
+                int index = s[j] - 'a';
+                if (node->child[index] == NULL) {
+                    break;
+                }
+                node = node->child[index];
+                if (node->isend) {
+                    dp[j + 1] = true;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
