0f9d41a66e5bd5353e5d48a04f6974354be58129
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-06-19 14:41:22 +0530

    Time: 0 ms (100%), Space: 12.7 MB (6.19%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,16 +1,55 @@
+class Node {
+public:
+    Node* child[26];
+    int childcount;
+    bool isEnd;
+    Node() {
+        isEnd = false;
+        childcount = 0;
+        for (int i = 0; i < 26; i++) {
+            child[i] = NULL;
+        }
+    }
+};
+class Trie {
+public:
+    Node* root;
+
+    Trie() { root = new Node(); }
+    void insert(string word) {
+        Node* node = root;
+        for (char ch : word) {
+            int i = ch - 'a';
+            if (node->child[i] == NULL) {
+                node->child[i] = new Node();
+                node->childcount++;
+            }
+            node = node->child[i];
+        }
+        node->isEnd = true;
+    }
+    string getLcp() {
+        Node* node = root;
+        string ans = "";
+        while (node->childcount == 1 && !node->isEnd) {
+            for (int i = 0; i < 26; i++) {
+                if (node->child[i]) {
+                    ans += char('a' + i);
+                    node = node->child[i];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        sort(begin(strs),end(strs));
-        string first=strs[0];
-        string last=strs.back();
-        string ans="";
-        for(int i=0;i<min(first.size(),last.size());i++){
-            if(first[i]!=last[i]){
-                break;
-            }
-            ans+=first[i];
+        Trie t;
+        for (auto ch : strs) {
+            t.insert(ch);
         }
-        return ans;
+        return t.getLcp();
     }
 };
