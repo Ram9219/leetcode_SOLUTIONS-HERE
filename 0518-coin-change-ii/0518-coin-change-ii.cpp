3060d17e7fa27f6f2f0cfdf5918a0da7cf95b75a
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-06-22 11:12:40 +0530

    Time: 6 ms (87.95%), Space: 10.3 MB (90.61%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<unsigned long long>dp(amount+1,0);
+        dp[0]=1;
+        for(auto &coin :coins){
+            for(int j=coin ; j<=amount ;j++){
+                dp[j]=dp[j] + dp[j-coin];
+            }
+        }
+        return (int)dp[amount];
+    }
+};
