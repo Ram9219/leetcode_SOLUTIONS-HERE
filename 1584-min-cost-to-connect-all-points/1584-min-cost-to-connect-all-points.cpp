e0cb7053b8514b478ec24e3131ccabc64001ac7b
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-06-24 17:41:12 +0530

    Time: 750 ms (20.49%), Space: 180.6 MB (13.48%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,25 +1,61 @@
+class Disjoint {
+public:
+    vector<int> rank, parent;
+    Disjoint(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+    int ult_parent(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = ult_parent(parent[node]);
+    }
+    bool Find_Union_By_Rank(int u, int v) {
+        int pu = ult_parent(u);
+        int pv = ult_parent(v);
+        if (pu == pv)
+            return false;
+        if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        } else if (rank[pv] < rank[pu]) {
+            parent[pv] = pu;
+        } else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+        return true;
+    }
+};
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& houses) {
-        // code here
-        int n=houses.size();
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
-        vector<int>vis(n,0);
-        pq.push({0,0});//{weight,node}
-        int sum=0;
-        while(!pq.empty()){
-            auto[wt,node]=pq.top();
-            pq.pop();
-            if(vis[node]==1)continue;
-            vis[node]=1;
-            sum+=wt;
-            for(int i=0;i<n;i++){
-                int newcost=abs(houses[node][0]-houses[i][0])+
-                abs(houses[node][1]-houses[i][1]);
-                pq.push({newcost,i});
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n=points.size();
+        vector<vector<int>>edges;
+        for(int i=0;i<n;i++){
+            for(int j=i+1 ;j<n ;j++){
+                int cost=abs(points[i][0]-points[j][0])+
+                abs(points[i][1]-points[j][1]);
+                edges.push_back({cost,i,j});//cost ,u ,v
             }
         }
-        return sum;
-    
+        sort(begin(edges),end(edges));
+        Disjoint ds(n);
+        int ans=0;
+        int count=0;//for finding the mst cycle 
+        for(auto &e:edges){
+            int cost=e[0];
+            int u=e[1];
+            int v=e[2];
+            if(ds.Find_Union_By_Rank(u,v)){
+                ans+=cost;
+                count++;
+            }
+            if(count==n-1)break;
+
+        }
+        return ans;
     }
 };
