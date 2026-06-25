4d765a1ccac109cc32f1fb319b883414b43868e0
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-06-25 16:10:42 +0530

    Time: 0 ms (100%), Space: 10.9 MB (65.93%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -6,29 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    // we will use the same logic as LNR
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        stack<TreeNode*> st;
-        TreeNode* node = root; // jisse current node track ho paye
-        while (true) {
-            if (node != NULL) {
-                st.push(node); // agar node null nahi hai that means koi value
-                               // hai push kado
-                node = node->left; // aur node ko left me badha do
-            } else {
-                if (st.empty() == true)
-                    break; // agar stack khatam ho chuka hai to ruk  kjao
-                node = st.top();
+        vector<int>ans;
+        if(!root)return ans;
+        stack<TreeNode*>st;
+        TreeNode*node=root;
+        while(true){
+            if(node!=NULL){
+                st.push(node);
+                node=node->left;
+            }else{
+                if(st.empty())break;
+                node=st.top();
                 st.pop();
                 ans.push_back(node->val);
-                node = node->right;
+                node=node->right;
             }
         }
         return ans;
