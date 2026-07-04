993b1b6ca8f2bf5f058c2edbb525b6c9fb92237b
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-07-04 21:21:34 +0530

    Time: 52 ms (79.45%), Space: 128.6 MB (83.59%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,53 +1,32 @@
-class Disjoint {
-public:
-    vector<int> rank, parent;
-    Disjoint(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        iota(begin(parent), end(parent), 0);
-    }
-    int ult_parent(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = ult_parent(parent[node]);
-    }
-    void Union_By_Rank(int u, int v) {
-        int pu = ult_parent(u);
-        int pv = ult_parent(v);
-        if (pu == pv)
-            return;
-        if (rank[pu] < rank[pv]) {
-            parent[pu] = pv;
-        } else if (rank[pv] < rank[pu]) {
-            parent[pv] = pu;
-        } else {
-            parent[pv] = pu;
-            rank[pu]++;
-        }
-    }
-};
-
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        Disjoint ds(n);
-        int ans = INT_MAX;
+
+        vector<pair<int, int>> adj[n + 1];
         for (auto& e : roads) {
             int u = e[0];
             int v = e[1];
-            ds.Union_By_Rank(u, v);
-        }
-
-         for (auto& e : roads) {
-            int u = e[0];
-            int v = e[1];
             int wt = e[2];
-            int p = ds.ult_parent(1);
-            if (ds.ult_parent(u) == p) {
-                ans = min(ans, wt);
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
+        }
+        queue<int> q;
+        q.push(1);
+
+        vector<int> vis(n + 1, 0);
+        int ans = INT_MAX;
+        vis[1] = 1;
+        while (!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            for (auto& it : adj[node]) {
+                ans = min(ans, it.second);
+                if (!vis[it.first]) {
+                    vis[it.first] = 1;
+                    q.push(it.first);
+                }
             }
         }
-    
         return ans;
     }
 };
