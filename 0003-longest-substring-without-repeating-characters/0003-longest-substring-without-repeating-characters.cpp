8ee5db45ff020ea6fe40f22cb7993a540d231809
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-08-02 19:31:13 +0530

    Time: 42 ms (10.36%), Space: 19.3 MB (8.78%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l=0,r=0,maxlen=0;
-        int n=s.length();
-        vector<int>freq(256,-1);
-        while(r<n){
-            if(freq[s[r]]!=-1 && freq[s[r]]>=l){
-                //duplicatre nahi hai matlab 
-                     l=freq[s[r]]+1;
+
+        int left = 0;
+        int ans = 0;
+        unordered_map<char, int> mp;
+        for (int right = 0; right < s.size(); right++) {
+            if (mp.count(s[right])) {
+                left = max(left, mp[s[right]] + 1);
             }
-            int len=r-l+1;
-            maxlen=max(maxlen,len);
-           freq[s[r]]=r;
-           r++;
+            mp[s[right]] = right;
+            ans = max(ans, right - left + 1);
         }
-        return maxlen;
+        return ans;
     }
 };
