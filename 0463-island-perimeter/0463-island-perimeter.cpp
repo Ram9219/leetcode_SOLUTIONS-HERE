f324c72343980f0f5a80ef1cb8f379c3daf8f226
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-09-14 16:12:48 +0530

    Time: 24 ms (11.03%), Space: 108.7 MB (8.83%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,22 +1,40 @@
 class Solution {
+
 public:
-    int islandPerimeter(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int peri=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    peri+=4;
-                    if(i>0 && grid[i-1][j]==1){
-                        peri-=2;
-                    }
-                    if(j>0 && grid[i][j-1]==1){
-                        peri-=2;
-                    }
+    int n, m;
+    vector<vector<int>> grid;
+    vector<vector<int>> vis;
+    int dfs(int row, int col) {
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, -1, 0, 1};
+        if (row < 0 || row >= n || col < 0 || col >= m || grid[row][col] == 0) {
+            return 1;
+        }
+        if (vis[row][col]) {
+            return 0;
+        }
+        vis[row][col] = 1;
+        int peri = 0;
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            peri += dfs(nrow, ncol);
+        }
+
+        return peri;
+    }
+    int islandPerimeter(vector<vector<int>>& g) {
+        grid = g;
+        n = grid.size();
+        m = grid[0].size();
+        vis.assign(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    return dfs(i, j);
                 }
             }
         }
-        return peri;
+        return 0;
     }
 };
